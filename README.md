6f1f95b4e6952b0b0df7a3d0c594285d6c1543bc
Rajinda Dissanayake <rajidissa@hotmail.com>
2024-09-15 17:03:49 +0100

    Input argument validation has been completed

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Assumptions
 
+1. No spaces are allowed in the file path argument
+2. The file path length cannot exceed 200 characters
---
+++ src/main/java/com/motlient/notification/util/AppValidator.java
@@ -0,0 +1,51 @@
+package com.motlient.notification.util;
+
+import com.motlient.notification.exceptions.AppValidationException;
+
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Validation class
+ */
+public class AppValidator {
+
+    public static final Logger LOGGER = Logger.getLogger(AppValidator.class.getName());
+
+    /**
+     * Validates the argument based on the following criteria:
+     * 1. The argument must contain only one value.
+     * 2. The argument must be a valid URL.
+     *
+     * @param args - input URL
+     * @throws AppValidationException if validation fails
+     */
+    public void validateJsonFilePath(String[] args) throws AppValidationException {
+        if (null == args || args.length == 0 || args[0].equals("")) {
+            throw new AppValidationException(AppConstants.MESSAGE_FILE_PATH_NOT_PROVIDED);
+        } else if (args.length > 1) {
+            throw new AppValidationException(AppConstants.MESSAGE_TOO_MANY_ARGUMENTS_ARE_PROVIDED);
+        } else if (validateUrlForSpecialCharacters(args[0])) {
+            throw new AppValidationException(AppConstants.MESSAGE_INVALID_FILE_PATH);
+        } else if (args[0].length() > 200) {
+            throw new AppValidationException(AppConstants.MESSAGE_FILE_PATH_LENGTH_EXCEEDED);
+        } else if (!validateUrlForValidUrlPatten(args[0])) {
+            throw new AppValidationException(AppConstants.MESSAGE_INVALID_URL_FORMAT);
+        }
+    }
+
+    private boolean validateUrlForSpecialCharacters(String url) {
+        return validateUrlWithRegExpression(AppConstants.COMMON_URL_CHARACTERS, url);
+    }
+
+    private boolean validateUrlForValidUrlPatten(String url) {
+        return validateUrlWithRegExpression(AppConstants.VALID_URL_PATTERN, url);
+    }
+
+    private boolean validateUrlWithRegExpression(String exp, String url) {
+        Pattern pattern = Pattern.compile(exp);
+        Matcher matcher = pattern.matcher(url);
+        return matcher.find();
+    }
+}
